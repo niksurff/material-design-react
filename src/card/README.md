265eb0086d34dd3e007f9f27af01c41e0cbaef8c
nikvm <dev@nikvm.me>
2015-07-20 23:47:25 +0200

    Add Card props for shorthand usage

--- src/card/README.md
+++ src/card/README.md
@@ -31,11 +31,35 @@ let myCard = (
 );
 ```
 
+You may also use a shorthand with properties.
+
+```javascript
+let myCard = (
+  <Card
+    shadow="2"
+    title="Look, a Card!"
+    titleLevel="3"
+    subtitle="It is, indeed."
+    actions={[<Button>Act on it.</Button>, <Button accent>Do it!</Button>]}
+    actionsBorder />
+);
+```
+
+
 ## Properties
 
 Property | Type | Default | Effect | Remarks
 -------- | -----| ------- | ------ | -------
-shadow | on of: 2, 3, 4, 6, 8 | undefined | Assigns shadow with variable depths | Depths 5 and 7 don't exist
+shadow | one of: 2, 3, 4, 6, 8 | undefined | Assigns shadow with variable depths | Depths 5 and 7 don't exist
+title | node | undefined | Applies provided nodes as heading | -
+titleLevel | one of: 1, 2, 3, 4, 5, 6 | 1 | Sets title's heading level | Sets html elements `<h1>` through `<h6>`
+titleBorder | bool| undefined | Gives title container a border | -
+subtitle | node | undefined | Applies provided node as subtitle | -
+supporting | node | undefined | Applies provided node as supporting text | -
+media | node | undefined | Includes provided node in media container | -
+actions | node | undefined | Includes provided node in actions container | -
+actionsBorder | bool | undefined | Gives actions container a border | -
+menu | node | undefined | Includes provided node in menu container | Usually a mdl/mdr icon button plus menu
 
 ## Children
 
@@ -49,4 +73,14 @@ shadow | on of: 2, 3, 4, 6, 8 | undefined | Assigns shadow with variable depths 
 
 
 ## Notes
-*none*
+
+When using `Card` with props the order in which children are rendered is as follows:
+1. `title`
+2. `subtitle`
+3. `supporting`
+4. `media`
+5. `children` (prop or actual)
+6. `actions`
+7. `menu`
+
+If your use case seeks a different order, use `Card` with children instead or mix as needed (as `children` will be rendered in any case).
--- src/card/index.jsx
+++ src/card/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import mdlHook from '../decorator/mdl-hook';
 import CardMenu from './menu';
 import CardMedia from './media';
 import CardTitle from './title';
 import CardActions from './actions';
 import CardTitleText from './title-text';
-import mdlHook from '../decorator/mdl-hook';
 import CardSubtitleText from './subtitle-text';
 import CardSupportingText from './supporting-text';
 
@@ -14,19 +14,104 @@ import CardSupportingText from './supporting-text';
   modifiers: [
     {
       prop: 'shadow',
-      classNameFn: (val) => `mdl-shadow--${val}dp`,
-      type: React.PropTypes.oneOf([2, 3, 4, 6, 8])
+      type: React.PropTypes.oneOf([2, 3, 4, 6, 8]),
+      classNameFn: (val) => `mdl-shadow--${val}dp`
     }
   ]
 })
 class Card extends React.Component {
+  static propTypes = {
+    title: React.PropTypes.node,
+    titleLevel: React.PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
+    titleBorder: React.PropTypes.bool,
+    subtitle: React.PropTypes.node,
+    supporting: React.PropTypes.node,
+    media: React.PropTypes.node,
+    actions: React.PropTypes.node,
+    actionsBorder: React.PropTypes.bool,
+    menu: React.PropTypes.node,
+    children: React.PropTypes.node
+  }
+
+  static defaultProps = {
+    titleLevel: 1
+  }
 
   render() {
-    return <div {...this.props}/>;
+    let {
+      title,
+      titleLevel,
+      titleBorder,
+      subtitle,
+      supporting,
+      media,
+      actions,
+      actionsBorder,
+      menu,
+      children,
+      ...otherProps
+    } = this.props;
+
+    let renderedTitle = renderTitle({title, titleLevel, titleBorder, subtitle});
+    let renderedSupporting = renderSupporting({supporting});
+    let renderedMedia = renderMedia({media});
+    let renderedActions = renderActions({actions, actionsBorder});
+    let renderedMenu = renderMenu({menu});
+
+    return (
+      <div {...otherProps}>
+        {renderedTitle}
+        {renderedSupporting}
+        {renderedMedia}
+        {children}
+        {renderedActions}
+        {renderedMenu}
+      </div>
+    );
   }
 }
 
-// Children
+function renderTitle(props) {
+  let Subtitle = props.title && props.subtitle
+    ? <CardSubtitleText children={props.subtitle} />
+    : null;
+  if (props.title) return (
+    <CardTitle border={props.titleBorder}>
+      <CardTitleText children={props.title} level={props.titleLevel} />
+      {Subtitle}
+    </CardTitle>
+  );
+  return null;
+}
+
+function renderSupporting(props) {
+  if (props.supporting) return (
+    <CardSupportingText children={props.supporting}/>
+  );
+  return null;
+}
+
+function renderMedia(props) {
+  if (props.media) return (
+    <CardMedia children={props.media}/>
+  );
+  return null;
+}
+
+function renderActions(props) {
+  if (props.actions) return (
+    <CardActions children={props.actions} border={props.actionsBorder}/>
+  );
+  return null;
+}
+
+function renderMenu(props) {
+  if (props.menu) return (
+    <CardMenu children={props.menu}/>
+  );
+  return null;
+}
+
 Card.Title = CardTitle;
 Card.TitleText = CardTitleText;
 Card.SubtitleText = CardSubtitleText;
