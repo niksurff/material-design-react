968275ef151d83b9a55d2888422ee2cdf3c6ddba
nikvm <dev@nikvm.me>
2015-07-23 20:44:36 +0200

    Use lib/ as place for non-component files

--- src/util/omit-obj-keys.js
+++
@@ -1,5 +0,0 @@
-import omit from 'omit';
-
-export default function omitObjKeys(props, obj) {
-  return omit(Object.keys(obj))(props);
-}
--- src/util/prepare-props.js
+++
@@ -1,9 +0,0 @@
-import mdlClassName from './mdl-class-name';
-import omitObjKeys from './omit-obj-keys';
-
-export default function prepareProps(props, config) {
-  return Object.assign(
-    omitObjKeys(props, config.modifierClassNameFns),
-    {className: mdlClassName(props, config)}
-  );
-}
