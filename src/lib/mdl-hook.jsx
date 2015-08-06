3504e0063f70fadff89f3e5edd4d4c93b1bd1147
nikvm <dev@nikvm.me>
2015-08-06 21:11:00 +0200

    Add inline docs

--- src/lib/mdl-hook.jsx
+++ src/lib/mdl-hook.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import prepareProps from './prepare-props';
 
+
 /**
- * Returns a decorator rendering Target with mdl className,
- * handles lifecycle hooks
- * @param  {Object} config   The config object
- * @return {Function}        The decorator function
+ * # mdlHook
+ *
+ * wraps `Target` in higher order Component which
+ * turns provided `config` into MDL CSS classes and provides lifecycle methods
+ * for upgrading/ downgrading a Component via
+ * [mdlComponentHandler](https://github.com/google/material-design-lite/blob/master/src/mdlComponentHandler.js).
+ *
+ * ## `config` Properties
+ *
+ * The `config` param takes the following properties:
+ * 1. `displayName`<String>: React display name of higher order element
+ * 2. `blockClassName`<String>: The Block -- as in BEM -- MDL CSS class (inc. js hook)
+ * 3. `modifiers`<Array>: An array of modifier objects configuring MDL modifier (BEM) CSS classes.
+ *
+ * ### `modifiers[i]` Properties
+ *
+ * A modifier object takes the following props:
+ * 1. `prop`<String>: Property name
+ * 2. `className`<String>: MDL CSS class (mutually exclusive with `classNameFn`, takes precedence)
+ * 3. `classNameFn`<Function>: A function returning MDL CSS class string, takes prop value as argument. ((mutually exclusive with `className`)
+ * 4. `type`<Function>: One of React's `PropType`s (or custom)
+ * 5. `default`<Any>: Default value (optional)
  */
 export default function mdlHook(config) {
   let {displayName, blockClassName, modifiers = []} = config;
